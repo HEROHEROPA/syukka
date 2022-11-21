6b1af9099176a3b7b3139a51914bc8ef0e4dbc21
HEROHEROPA <61680482+HEROHEROPA@users.noreply.github.com>
2022-11-21 10:31:12 +0900

    デプロイ切り替え設定　バリデーションスキップ　--no-verify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
 
+  before_action :basic_auth
+  
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER_SYUKKA'] && password == ENV['BASIC_AUTH_PASSWORD_SYUKKA']
+    end
+  end
+
   # after_action :set_csrf_token_header, if: :to_stocks_controller?
 
   # def set_csrf_token_header
